af9a6e04fcb073b652d6e36076833c706f51fcfb
JAYESH SASTURKAR <jayesh.sasturkar29@gmail.com>
2024-11-10 17:08:32 +0530

    Added absolute import configurations using vite.config and jsconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 
+import Home from 'Pages/Home';
+
 function App() {
 
   return (
     <>
-      Hello
+      <Home/>
     </>
   );
 }
@@ -12,4 +14,4 @@ function App() {
 export default App;
 
 // React + Tailwind + DaisyUI + Redux Toolkit + React Redux + React Hot Toast + React Icons 
-// SimpleImportSort + AutoSemiColon + React Router DOM 
+// SimpleImportSort + AutoSemiColon + React Router DOM + Folder Setup + Absolute Paths for Importing Folders
